adee4ef986e2d8e0772f874009f41c7ab90bcc6f
Count V. Dracula <dracula@in.waw.pl>
2019-02-15 11:08:53 +0100

    Add a docstring that says what needs to be done

--- maxima.py
+++ maxima.py
@@ -1,5 +1,11 @@
 # lbasdfasdf sadfasdf
 
 def find_maxima(arr):
-    return [1]
+    """Returns the positions of the maximum values in the array
+    arr.
 
+    Example:
+    find_maxima([1, 2, 1]) → [1]
+    find_maxima([2, 0, 0, -2, 2]) → [0, 4]
+    """
+    return [0]
