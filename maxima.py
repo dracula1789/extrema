bcce7728615f17ff545f5f7004b913fa1229a903
Count V. Dracula <dracula@in.waw.pl>
2019-02-15 11:12:29 +0100

    First version of find_maxima

--- maxima.py
+++ maxima.py
@@ -8,4 +8,10 @@ def find_maxima(arr):
     find_maxima([1, 2, 1]) → [1]
     find_maxima([2, 0, 0, -2, 2]) → [0, 4]
     """
-    return [0]
+    ans = []
+    for i in range(len(arr)):
+        if (arr[i] > arr[i-1] and
+            arr[i] > arr[i+1]):
+            ans.append(i)
+    return ans
+
