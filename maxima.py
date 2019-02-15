fa46860f5ed1e6bb98dd40ebd796aac81ce6b86a
Count V. Dracula <dracula@in.waw.pl>
2019-02-15 11:26:14 +0100

    Fix bug when index is too large

--- maxima.py
+++ maxima.py
@@ -12,7 +12,7 @@ def find_maxima(arr):
     ans = []
     for i in range(len(arr)):
         if (arr[i] > arr[i-1] and
-            arr[i] > arr[i+1]):
+            (i >= len(arr) or arr[i] > arr[i+1])):
             ans.append(i)
     return ans
 
